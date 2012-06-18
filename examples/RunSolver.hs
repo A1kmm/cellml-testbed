b8f5b88eec9a021b98cca8b43a481c6a51dc6c4b
Andrew Miller <ak.miller@auckland.ac.nz>
2012-06-18 19:29:03 +1200

    Get RunSolver to the point where it compiles and attempts to load a model. We don't have any models coded against CellML 1.2 to test it with yet.

--- examples/RunSolver.hs
+++ examples/RunSolver.hs
@@ -1,44 +1,75 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
 import Data.CellML12.ModelSolver
+import Data.CellML12.Parser (InvalidCellML(..))
 import Data.CellML12.ToSimplifiedModel
+import Data.CellML12.SimplifiedModel
+import Data.CellML12.ModelLoader (runLoadModels)
 import System.Console.CmdArgs
+import Data.List
+import Data.Maybe
+import Control.Monad
+import Control.Monad.IO.Class
+import qualified Data.Map as M
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import qualified Data.Array.IArray as A
 
-data SolverOpts { modelURL :: String, componentOfBoundVariable :: String, boundVariableName :: String, 
-                  lowerLimit :: Double, upperLimit :: Double,
-                  relativeTolerance :: Double, absoluteTolerance :: Double } deriving (Show, Data, Typeable)
+data SolverOpts = SolverOpts {
+  modelURL :: String, componentOfBoundVariable :: String,
+  boundVariableName :: String, lowerLimit :: Double, upperLimit :: Double,
+  relativeTolerance :: Double, absoluteTolerance :: Double } deriving (Show, Data, Typeable)
 solverOpts = SolverOpts { modelURL = def &= help "URL of the model to simulate",
                           componentOfBoundVariable = def &= help "The component containing the bound variable for the integral",
-                          boundVariableName = def &= help "The name of the bound variable" &= opt "time",
-                          lowerLimit = def &= help "The lower limit of the integral to compute" &= opt 0,
-                          upperLimit = def &= help "The upper limit of the integral to compute" &= opt 10,
-                          relativeTolerance = def &= help "The relative solver tolerance" &= opt 1E-6,
-                          absoluteTolerance = def &= help "The absolute solver tolerance" &= opt 1E-6 }
+                          boundVariableName = def &= help "The name of the bound variable" &= opt ("time" :: String),
+                          lowerLimit = def &= help "The lower limit of the integral to compute" &= opt (0 :: Double),
+                          upperLimit = def &= help "The upper limit of the integral to compute" &= opt (10 :: Double),
+                          relativeTolerance = def &= help "The relative solver tolerance" &= opt (1E-6 :: Double),
+                          absoluteTolerance = def &= help "The absolute solver tolerance" &= opt (1E-6 :: Double) }
 
 rightOrFail _ (Right x) = return x
 rightOrFail ef (Left e) = fail $ ef e
 justOrFail _ (Just x) = return x
 justOrFail m _ = fail m
 
-findTime :: SimplifiedModel -> SolverOpts -> VariableID
+findTime :: SimplifiedModel -> SolverOpts -> Maybe VariableID
 findTime (SimplifiedModel { variableInfo = VariableInfo vmap}) (SolverOpts { boundVariableName = n,
                                                                              componentOfBoundVariable = c }) =
-  find (any ( . snd) . snd) (M.toList vmap)
+  case find (any ((==(ModelPathComponent c (ComponentPathVariable n))) . fst) . snd) 
+       (M.toList vmap)
+  of
+    Just (vid, _) -> return vid
+    Nothing -> Nothing
 
 makeCSVHeading :: SimplifiedModel -> M.Map (VariableID, Int) Int -> LBS.ByteString
+makeCSVHeading (SimplifiedModel { variableInfo = VariableInfo vimap }) vToID =
+  -- Reverse the variable map to yield index => variable path...
+  let
+    idxToPath = M.fromList . mapMaybe (\((v,deg), idx) -> liftM (((,)idx) . (flip (,) deg) . fst . head) (M.lookup v vimap)) . M.toList $ vToID
+  in
+   LBS.intercalate "," (map (\i -> case M.lookup i idxToPath of
+                                Nothing -> "Internal variable"
+                                Just (vpath, deg) ->
+                                  displayPath vpath `LBS.append`
+                                  (LBS.concat (take 10 (repeat "'")))
+                            ) [0..(M.size vToID)])
+
+displayPath (ModelPathComponent s cp) = (LBS.pack s) `LBS.append` ("/" `LBS.append` displayCPath cp)
+displayPath (ModelPathImport i mp) = (LBS.pack i) `LBS.append` ("/" `LBS.append` displayPath mp)
+displayPath _ = ""
+displayCPath (ComponentPathVariable s) = LBS.pack s
+displayCPath _ = ""
 
 main = do
   cmd <- cmdArgs solverOpts
-  m <- rightOrFail (\InvalidCellML e -> "Model load failed: " ++ e) =<<
+  m <- rightOrFail (\(InvalidCellML e) -> "Model load failed: " ++ e) =<<
        runLoadModels (buildSimplifiedModel (modelURL cmd))
   vt <- justOrFail "Cannot find specified bound variable for integration" $ findTime m cmd
   rightOrFail (\x -> "Error running model: " ++ x) =<<
-    runSolverOnDAESimplifiedModel m (DAEIntegrationSetup { daeModel = m, daeBoundVariable = vt }) $ do
-      DAEIntegrationResult idx rows <- solveModelWithParameters
+    (runSolverOnDAESimplifiedModel m (DAEIntegrationSetup { daeModel = m, daeBoundVariable = vt }) $ do
+      DAEIntegrationResults idx rows <- solveModelWithParameters
              (DAEIntegrationProblem { daeParameterOverrides = [],
                                       daeBVarRange = (lowerLimit cmd, upperLimit cmd),
                                       daeRelativeTolerance = relativeTolerance cmd,
                                       daeAbsoluteTolerance = absoluteTolerance cmd })
-      LBS.putStrLn $ makeCSVHeading m idx
-      mapM (LBS.putStrLn . LBS.intersperse ',' . map (LBS.pack . show) . A.elems . snd) rows
+      liftIO . LBS.putStrLn $ makeCSVHeading m idx
+      mapM (liftIO . LBS.putStrLn . LBS.intercalate "," . map (LBS.pack . show) . A.elems . snd) rows
+    )
